72dd9a15dff1f88119db4a1545f6a711af35ceb6
Aaron Nies <aaron.nies@digitalglobe.com>
2017-10-16 11:10:01 -0400

    Convert swagger notes from HTML to Markdown

--- plugins/omar-wmts-plugin/grails-app/controllers/omar/wmts/WmtsController.groovy
+++ plugins/omar-wmts-plugin/grails-app/controllers/omar/wmts/WmtsController.groovy
@@ -16,69 +16,48 @@ class WmtsController {
     def webMapTileService
 //    @Secured( ['IS_AUTHENTICATED_ANONYMOUSLY'] )
     @ApiOperation( value = "OGC WMTS service", produces = 'application/vnd.ogc.wms_xml',
-            notes=""" This is the main entry point for OGC WMTS services.
-<br>
-<br>
-    <ul>
-        <li>
-            <b>service</b><p/>
-            Is a required parameter that has a fixed value of WMTS
-        </li>
-        <br><br>
-        <li>
-            <b>version</b><p/>
-            Version 1.0.0 is supported
-        </li>
-        <br><br>
-        <li>
-            <b>request</b><p/>
-            The only supported requests are GetCapabilities or GetTile.  If GetCapabilities is selected then
-            the only required parameters that must be filled are service, version, and request.  If GetTile is selected then you must
-            also enter values for fields format, tileRow, tileCol, tileMatrix, and tileMatrixSet
-        </li>
-        <br><br>
-        <li>
-            <b>layer</b><p/>
-            This is the layer name for the GetTile call.
-        </li>
-        <br><br>
-        <li>
-            <b>layer</b><p/>
-            This is the layer name for the GetTile.
-        </li>
-        <br><br>
-        <li>
-            <b>format</b><p/>
-            Defines the output format of the tile if the request is GetTile.  The currently supported formats
-            can be image/jpeg or image/png
-        </li>
-        <br><br>
-        <li>
-            <b>tileRow</b><p/>
-            Tile numbering starts in the upper left corner of a pyramid.  So tileRow = 0 refers to the top row of
-            tiles.  This is the tileRow index for the tileMatrix.
-        </li>
-        <br><br>
-        <li>
-            <b>tileCol</b><p/>
-            Tile numbering starts in the upper left corner of a pyramid.  So tileCol = 0 refers to the first column of
-            tiles.  This is the tileCol index for the tileMatrix.
-        </li>
-        <br><br>
-        <li>
-            <b>tileMatrix</b><p/>
-            Character String type, not empty.  This is string identifier.  On our server the capabilities will list this as
-            identifiers from 0 .. maxLeve.  This will have a more 1 to 1 mapping to the level/row/col format where the tileMatrix id refers
-            to the level and level 0 is the coarsest resolution down to the highest resolution of maxLevel.
-        </li>
-        <br><br>
-        <li>
-            <b>tileMatrixSet</b><p/>
-            Character String type, not empty. This is the Set idientifier.  This can be read from the capabilites.  By default we
-            have the WorldGeographic is a default MatrixSet identifier
-        </li>
-        <br>
-    </ul>
+            notes="""
+This is the main entry point for OGC WMTS services.  
+
+*   **service**
+
+    Is a required parameter that has a fixed value of WMTS
+
+*   **version**
+
+    Version 1.0.0 is supported
+
+*   **request**
+
+    The only supported requests are GetCapabilities or GetTile. If GetCapabilities is selected then the only required parameters that must be filled are service, version, and request. If GetTile is selected then you must also enter values for fields format, tileRow, tileCol, tileMatrix, and tileMatrixSet
+
+*   **layer**
+
+    This is the layer name for the GetTile call.
+
+*   **layer**
+
+    This is the layer name for the GetTile.
+
+*   **format**
+
+    Defines the output format of the tile if the request is GetTile. The currently supported formats can be image/jpeg or image/png
+
+*   **tileRow**
+
+    Tile numbering starts in the upper left corner of a pyramid. So tileRow = 0 refers to the top row of tiles. This is the tileRow index for the tileMatrix.
+
+*   **tileCol**
+
+    Tile numbering starts in the upper left corner of a pyramid. So tileCol = 0 refers to the first column of tiles. This is the tileCol index for the tileMatrix.
+
+*   **tileMatrix**
+
+    Character String type, not empty. This is string identifier. On our server the capabilities will list this as identifiers from 0 .. maxLeve. This will have a more 1 to 1 mapping to the level/row/col format where the tileMatrix id refers to the level and level 0 is the coarsest resolution down to the highest resolution of maxLevel.
+
+*   **tileMatrixSet**
+
+    Character String type, not empty. This is the Set idientifier. This can be read from the capabilites. By default we have the WorldGeographic is a default MatrixSet identifier
 """
     )
     @ApiImplicitParams( [
@@ -135,26 +114,20 @@ class WmtsController {
     }
 //    @Secured( ['IS_AUTHENTICATED_ANONYMOUSLY'] )
     @ApiOperation( value = "Get the capabilities of the server", produces = 'application/vnd.ogc.wms_xml',
-            notes=""" Will return the capabilities of the server.
-<br>
-<br>
-    <ul>
-        <li>
-            <b>service</b><p/>
-            Is a required parameter that has a fixed value of WMTS
-        </li>
-        <br><br>
-        <li>
-            <b>version</b><p/>
-            Version 1.0.0 is supported
-        </li>
-        <br><br>
-        <li>
-            <b>request</b><p/>
-            This is special method and only services GetCapabilities.
-        </li>
-        <br>
-    </ul>
+            notes="""
+Will return the capabilities of the server.  
+
+*   **service**
+
+    Is a required parameter that has a fixed value of WMTS
+
+*   **version**
+
+    Version 1.0.0 is supported
+
+*   **request**
+
+    This is special method and only services GetCapabilities.
 """
     )
     @ApiImplicitParams( [
@@ -193,61 +166,44 @@ class WmtsController {
 
 //    @Secured( ['IS_AUTHENTICATED_ANONYMOUSLY'] )
     @ApiOperation( value = "Get an image tile from the server", produces = 'application/vnd.ogc.wms_xml',
-            notes=""" This is the main entry point for OGC WMTS services.
-<br>
-<br>
-    <ul>
-        <li>
-            <b>service</b><p/>
-            Is a required parameter that has a fixed value of WMTS
-        </li>
-        <br><br>
-        <li>
-            <b>version</b><p/>
-            Version 1.0.0 is supported
-        </li>
-        <br><br>
-        <li>
-            <b>request</b><p/>
-            This is special method and only services GetTile.
-        </li>
-        <br><br>
-        <li>
-            <b>layer</b><p/>
-            This is the layer name for the GetTile call.
-        </li>
-        <br><br>
-        <li>
-            <b>format</b><p/>
-            Defines the output format of the tile if the request is GetTile
-        </li>
-        <br><br>
-        <li>
-            <b>tileRow</b><p/>
-            Tile numbering starts in the upper left corner of a pyramid.  So tileRow = 0 refers to the top row of
-            tiles.  This is the tileRow index for the tileMatrix.
-        </li>
-        <br><br>
-        <li>
-            <b>tileCol</b><p/>
-            Tile numbering starts in the upper left corner of a pyramid.  So tileCol = 0 refers to the first column of
-            tiles.  This is the tileCol index for the tileMatrix.
-        </li>
-        <br><br>
-        <li>
-            <b>tileMatrix</b><p/>
-            Character String type, not empty.  This is string identifier.  On our server the capabilities will list this as
-            identifiers from 0 .. maxLeve.  This will have a more 1 to 1 mapping to the level/row/col format where the tileMatrix id refers
-            to the level and level 0 is the coarsest resolution down to the highest resolution of maxLevel.
-        </li>
-        <br><br>
-        <li>
-            <b>tileMatrixSet</b><p/>
-            Character String type, not empty. This is the Set idientifier.  This can be read from the capabilites.  By default we
-            have the WorldGeographic is a default MatrixSet identifier
-        </li>
-        <br>
-    </ul>
+            notes="""
+This is the main entry point for OGC WMTS services.  
+
+*   **service**
+
+    Is a required parameter that has a fixed value of WMTS
+
+*   **version**
+
+    Version 1.0.0 is supported
+
+*   **request**
+
+    This is special method and only services GetTile.
+
+*   **layer**
+
+    This is the layer name for the GetTile call.
+
+*   **format**
+
+    Defines the output format of the tile if the request is GetTile
+
+*   **tileRow**
+
+    Tile numbering starts in the upper left corner of a pyramid. So tileRow = 0 refers to the top row of tiles. This is the tileRow index for the tileMatrix.
+
+*   **tileCol**
+
+    Tile numbering starts in the upper left corner of a pyramid. So tileCol = 0 refers to the first column of tiles. This is the tileCol index for the tileMatrix.
+
+*   **tileMatrix**
+
+    Character String type, not empty. This is string identifier. On our server the capabilities will list this as identifiers from 0 .. maxLeve. This will have a more 1 to 1 mapping to the level/row/col format where the tileMatrix id refers to the level and level 0 is the coarsest resolution down to the highest resolution of maxLevel.
+
+*   **tileMatrixSet**
+
+    Character String type, not empty. This is the Set idientifier. This can be read from the capabilites. By default we have the WorldGeographic is a default MatrixSet identifier
 """
     )
     @ApiImplicitParams( [
@@ -318,7 +274,6 @@ class WmtsController {
             notes="""
 This is a simple service call that simplifies the layer listing to just dump the wmts layer table
 to a json formatted result
-<br>
 """
     )
     @ApiImplicitParams( [
